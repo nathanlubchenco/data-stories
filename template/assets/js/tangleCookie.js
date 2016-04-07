6fc8f18cf8e1220f4b972327670def4bdf4968bf
nathanlubchenco <nathanlubchenco@gmail.com>
2016-04-07 02:31:52 -0600

    initial sketch of template

---
+++ template/assets/js/tangleCookie.js
@@ -0,0 +1,18 @@
+(function(){
+    
+    /*
+    //TODO make work here instead of inline
+    console.log("loaded tangleCookie.js");
+    var tangle = new Tangle (document.getElementById("calorieCalculator"), {
+        initialize: function () {
+            this.cookies = 3;
+            this.caloriesPerCookie = 50;
+            this.dailyCalories = 2100;
+        },
+        update: function () {
+            this.calories = this.cookies * this.caloriesPerCookie;
+            this.dailyPercent = 100 * this.calories / this.dailyCalories;
+        }
+    });
+    */
+})();
